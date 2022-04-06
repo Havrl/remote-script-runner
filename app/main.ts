5e9b66bf5254e3055b95aa1a19e2714c5d8056dc
Sergey Kosik <sergey.kosik@ocrex.com>
2022-04-06 09:38:55 +0100

    Bump version to 1.0.1

--- app/main.ts
+++ app/main.ts
@@ -27,7 +27,7 @@ function createWindow(): BrowserWindow {
       allowRunningInsecureContent: serve ? true : false,
       contextIsolation: false, // false if you want to run e2e test with Spectron
     },
-    resizable: true,
+    resizable: serve ? true :false,
   });
 
   if (serve) {
